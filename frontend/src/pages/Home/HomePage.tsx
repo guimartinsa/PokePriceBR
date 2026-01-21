e1be24d63d4356a4eddf89f904eeec077d7fffeb
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-01-21 01:31:39 -0300

    global css

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,14 +1,17 @@
 // pages/Home/HomePage.tsx
+// pages/Home/HomePage.tsx
 import { Hero } from "./Hero";
 import { StatsBar } from "./StatsBar";
 import { RecentCards } from "./RecentCards";
+import "../../styles/global.css";
+//import "./home.css"
 
 export default function HomePage() {
     return (
-        <>
+        <div className="home">
             <Hero />
             <StatsBar />
             <RecentCards />
-        </>
+        </div>
     );
 }
