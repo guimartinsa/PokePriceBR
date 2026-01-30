d3022041f7dce882d6a134ba932cdc7c53c79c11
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-01-29 22:57:46 -0300

    icon botton bar

--- frontend/src/components/layout/BottomBar.tsx
+++ frontend/src/components/layout/BottomBar.tsx
@@ -1,5 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import  IconeBusca from "../../assets/icons/busca-icon.svg";
+import IconeBusca from "../../assets/icons/busca-icon.svg";
+import IconeCamera from "../../assets/icons/camera-pokemon.svg"; 
+import IconeColecao from "../../assets/icons/colecaoicon.svg";
+import IconHome from "../../assets/icons/home-icon.svg";
+import PocketIcon from "../../assets/icons/poket9-logo.svg"
+
+
 import "../../styles/global.css"
 
 
@@ -8,19 +14,33 @@ export default function BottomBar() {
 
     return (
         <nav className="bottom-bar">
-            <button onClick={() => navigate("/")}>🏠 Home</button>
-            <button onClick={() => navigate("/cards")}>
-                <img src={IconeBusca} alt="Buscar" width={25} height={25} />
-                Busca
+            <button onClick={() => navigate("/")}>
+                <img src={IconHome} alt="" width={35} height={35} />
+                
 
+            </button>
+
+            <button onClick={() => navigate("/cards")}>
+                <img src={IconeBusca} alt="Buscar" width={35} height={35} />
+                
             </button>
 
             {/* Botão central da câmera */}
             <button className="scan-button" onClick={() => navigate("/scan")}>
-                📷
+                <img src={IconeCamera} alt="" width={45} height={45} />
+                
             </button>
 
-            <button onClick={() => navigate("/collection")}>💼 Coleção</button>
+            <button>
+                <img src= {PocketIcon} alt="" width={35} height={35}/>
+                
+            </button>
+
+            <button onClick={() => navigate("/collection")}>
+                <img src={IconeColecao} alt="" width={35} height={35}/>
+                
+            </button>
         </nav>
     );
 }
+
