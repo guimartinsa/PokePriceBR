6ec989bbc909875b78fe4d16e1edda099934da5f
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-02-01 03:56:07 -0300

    config final de login de usuario

--- backend/pokepricebr/settings.py
+++ backend/pokepricebr/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 # SEGURANÇA / DEV
 # =========================
-SECRET_KEY = 'django-insecure-dev-key'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = True
 
