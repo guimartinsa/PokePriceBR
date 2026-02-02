78e3d069cb519e0b61a025a70c4a66d6a1bb3679
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-02-02 01:37:11 -0300

    fix url api render

--- backend/pokepricebr/urls.py
+++ backend/pokepricebr/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.http import JsonResponse
+
+
+def api_root(request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('api/', include('cards.urls')),
