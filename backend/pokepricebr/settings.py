751b08b354c2d75e2b701f26eb3a1f2e01cbf99c
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-02-01 22:33:14 -0300

    falha render

--- backend/pokepricebr/settings.py
+++ backend/pokepricebr/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # STATIC FILES
 # =========================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
