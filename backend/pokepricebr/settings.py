3fab85c0cd7e64a2f052fe1e47a1f68d49eaf0ab
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-02-10 01:03:30 -0300

    ipv4 render

--- backend/pokepricebr/settings.py
+++ backend/pokepricebr/settings.py
@@ -109,9 +109,10 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT", "5432"),
-        "CONN_MAX_AGE": 60,
+        "CONN_MAX_AGE": 600,
         "OPTIONS": {
             "sslmode": "require",
+            "target_session_attrs": "read-write",
         },
     }
 }
