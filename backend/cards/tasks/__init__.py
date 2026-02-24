0890c270fb0091448f822fb7813f23106f403fb2
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-02-24 15:08:11 -0300

    PRECO

--- backend/cards/tasks/__init__.py
+++ backend/cards/tasks/__init__.py
@@ -1,13 +1,26 @@
 from .atualizar_preco_carta import atualizar_preco_carta_task
+from .atualizar_precos_set_task import atualizar_precos_set_task
+from .atualizar_todas_cartas import atualizar_todas_cartas
+from .import_cards import import_cards_from_set_task
+from .import_sets import import_series_from_tcgdex_task, import_sets_from_tcgdex_task
 from .price_updates import (
     atualizar_preco_task,
     atualizar_colecao_task,
     atualizar_cartas_madrugada,
 )
+from .update_card_from_tcgdex import update_card_from_tcgdex_task
+from .update_set_cards_from_tcgdex import update_set_cards_from_tcgdex_task
 
 __all__ = [
     "atualizar_preco_carta_task",
+    "atualizar_precos_set_task",
+    "atualizar_todas_cartas",
+    "import_cards_from_set_task",
+    "import_series_from_tcgdex_task",
+    "import_sets_from_tcgdex_task",
     "atualizar_preco_task",
     "atualizar_colecao_task",
     "atualizar_cartas_madrugada",
+    "update_card_from_tcgdex_task",
+    "update_set_cards_from_tcgdex_task",
 ]
