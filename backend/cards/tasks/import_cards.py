5a29004e10ebc13d34b17f75307201434552027a
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-04-02 12:12:12 -0300

    raridade import

--- backend/cards/tasks/import_cards.py
+++ backend/cards/tasks/import_cards.py
@@ -6,4 +6,4 @@ from cards.services.import_cards import import_cards_from_tcgdex_set
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=10, retry_kwargs={"max_retries": 3})
 def import_cards_from_set_task(self, set_id: int):
     set_obj = Set.objects.get(id=set_id)
-    return import_cards_from_tcgdex_set(set_obj)
+    return import_cards_from_tcgdex_set(set_obj)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
